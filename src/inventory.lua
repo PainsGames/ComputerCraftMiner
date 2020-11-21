e1dc6287f6a6f495e34cd18b1d7021d04db700e1
Pain-in-the <69160979+PainsGames@users.noreply.github.com>
2020-11-21 03:06:16 +0000

    Expanding inventory functionality.

--- src/inventory.lua
+++ src/inventory.lua
@@ -7,11 +7,22 @@
 -- Item tags are in the expected format of "mod:item" so
 --  minecraft sand is "minecraft:sand"
 -- @return an array of indexs of slots containing the target items.
+
+local inventorySpaces = 16
+
+local function getItemDetails(slot)
+    return turtle.getItemDetail(slot)
+end
+
+local function getAllItemDetails(slot)
+    return turtle.getItemDetail(slot, true)
+end
+
 local function findInventorySlotsWithItem(itemTag)
     
     local slots = {}
 
-    for i=1,16 do
+    for i=1,inventorySpaces do
         local item = turtle.getItemDetail(i)
         if item ~= nil and item.name == itemTag then
             slots[#slots + 1] = i
@@ -30,6 +41,36 @@ local function isInventorySlotFull(slotNumber)
     end
 
     return isFull
+end
+
+local function getEmptySlots() 
+
+    local isThereAnEmptySlot = false
+
+    for i=1,inventorySpaces do
+        if getItemDetails(i) == nil then
+            isThereAnEmptySlot = true
+            break
+        end
+    end
+
+    return isThereAnEmptySlot
+end
+
+local function canInventoryFitBlock(itemTag)
+
+    local canFitBlock = false
+    
+        for i=1,inventorySpaces do
+        if isInventorySlotFull(i) == nil then
+            canFitBlock = true
+            break
+        else 
+            
+        end
+
+        return canFitBlock
+    end
 end
 
 -- [[ Public Api ]] --
--- src/tunnel.lua
+++ src/tunnel.lua
@@ -2,5 +2,18 @@ require "movements"
 require "actions" 
 require "inventory" 
 
+local height = 3
+local width = 3
+
+
+
 move.forward()
 
+-- [[ Helper Functions ]] --
+function mineBlock()
+    local blockToMine = inspect.front()
+
+    if blockToMine ~= nil then
+        dig.forward()
+    end
+end
